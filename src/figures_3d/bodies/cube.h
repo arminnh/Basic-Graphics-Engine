491915551ece60d71cad5d7f32fae1bb18cdcdec
Armin Halilovic <armin.halilovic@artiq.eu>
2019-07-09 19:04:10 +0200

    Cube construction

--- src/figures_3d/bodies/cube.h
+++ src/figures_3d/bodies/cube.h
@@ -16,7 +16,21 @@ public:
     Cube(ini::Section &config)
         : Figure(config)
     {
+        this->points.push_back(Vector3D::point(1, -1, -1));
+        this->points.push_back(Vector3D::point(1, -1, 1));
+        this->points.push_back(Vector3D::point(-1, -1, 1));
+        this->points.push_back(Vector3D::point(-1, -1, -1));
+        this->points.push_back(Vector3D::point(1, 1, -1));
+        this->points.push_back(Vector3D::point(1, 1, 1));
+        this->points.push_back(Vector3D::point(-1, 1, 1));
+        this->points.push_back(Vector3D::point(-1, 1, -1));
 
+        this->faces.push_back(Face(std::vector<int>{4, 5, 6, 7})); // back
+        this->faces.push_back(Face(std::vector<int>{3, 2, 6, 7})); // left side
+        this->faces.push_back(Face(std::vector<int>{0, 4, 7, 3})); // bottom
+        this->faces.push_back(Face(std::vector<int>{0, 1, 2, 3})); // front
+        this->faces.push_back(Face(std::vector<int>{0, 4, 5, 1})); // right side
+        this->faces.push_back(Face(std::vector<int>{1, 5, 6, 2})); // top
     }
 };
 
