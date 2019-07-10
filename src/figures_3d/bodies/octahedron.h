c09d8096b7e62c31a1cc79196f2c25bb035e3901
Armin Halilovic <armin.halilovic@artiq.eu>
2019-07-10 20:48:28 +0200

    Octahedron construction

--- src/figures_3d/bodies/octahedron.h
+++ src/figures_3d/bodies/octahedron.h
@@ -16,7 +16,24 @@ public:
     Octahedron(ini::Section &config)
         : Figure(config)
     {
+        this->points.push_back(Vector3D::point(0, 0, -1)); // bottom point
+        this->points.push_back(Vector3D::point(1, 0, 0));
+        this->points.push_back(Vector3D::point(0, -1, 0));
+        this->points.push_back(Vector3D::point(-1, 0, 0));
+        this->points.push_back(Vector3D::point(0, 1, 0));
+        this->points.push_back(Vector3D::point(0, 0, 1)); // top point
 
+        // bottom half
+        this->faces.push_back(Face(std::vector<int>{3, 0, 2}));
+        this->faces.push_back(Face(std::vector<int>{4, 0, 3}));
+        this->faces.push_back(Face(std::vector<int>{2, 0, 1}));
+        this->faces.push_back(Face(std::vector<int>{1, 0, 4}));
+
+        // top half
+        this->faces.push_back(Face(std::vector<int>{3, 5, 2}));
+        this->faces.push_back(Face(std::vector<int>{4, 5, 3}));
+        this->faces.push_back(Face(std::vector<int>{2, 5, 1}));
+        this->faces.push_back(Face(std::vector<int>{1, 5, 4}));
     }
 };
 
